83d292ef3aa2128fdd0aa3f58eca63c030d9e16d
Amir Hadi <amirhossein.hadi@gmail.com>
2020-08-21 15:06:09 -0700

    wristManeuver experiments added

--- dVRK/psm_wrist_maneuver.py
+++ dVRK/psm_wrist_maneuver.py
@@ -17,7 +17,7 @@ for item in start_pos:
     p.move_joint_one(float(item),axis,interpolate = True, blocking = True)
     axis+=1
 
-p.close_jaw(interpolate = True, blocking = True)
+p.move_jaw(0,interpolate = False, blocking = False)
 cycleNum = 1
 
 axisIndex = [3,4,5];
@@ -53,7 +53,7 @@ for counter in range(len(axisIndex)):
                 p.move_joint_one(float(item),axisIndex[counter],interpolate = False, blocking = False)
                 time.sleep(Ts)
 
-jaw_pos = [0,math.pi/2,0,-10/180*math.pi,0]
+jaw_pos = [0,math.pi/3,0,-10/180*math.pi,0]
 for value in jaw_pos:
     q0 = [p.get_current_jaw_position()]
     q1 = [value]
